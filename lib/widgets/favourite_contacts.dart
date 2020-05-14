c875c167b4dc234ecf2ffc1d9d41b250f8dbac83
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-14 17:18:22 +0530

    add some padding to the favorite container

--- lib/widgets/favourite_contacts.dart
+++ lib/widgets/favourite_contacts.dart
@@ -3,60 +3,65 @@ import '../models/message_model.dart';
 
 class FavouriteContacts extends StatelessWidget {
   Widget build(context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            //to leave a space between text and row
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'Favourite Contacts',
-                style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.0,
-                ),
-              ),
-              IconButton(
-                icon: Icon(Icons.more_horiz),
-                iconSize: 30.0,
-                color: Colors.blueGrey,
-                onPressed: () {},
-              )
-            ],
-          ),
-        ),
-        Container(
-          height: 120.0,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            padding: EdgeInsets.only(left: 10.0),
-            itemBuilder: (context, index) {
-              return Column(children: <Widget>[
-                CircleAvatar(
-                  radius: 35.0,
-                  backgroundImage: AssetImage(favorites[index].imageUrl),
-                ),
-                SizedBox(
-                  height: 6.0,
-                ),
-                Text(
-                  favorites[index].name,
-                  style: TextStyle(
-                    color: Colors.blueGrey,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
+    return Padding(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                //to leave a space between text and row
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Favourite Contacts',
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0,
+                    ),
                   ),
-                ),
-              ]);
-            },
-            itemCount: favorites.length,
-          ),
-        )
-      ],
-    );
+                  IconButton(
+                    icon: Icon(Icons.more_horiz),
+                    iconSize: 30.0,
+                    color: Colors.blueGrey,
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 120.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.only(left: 10.0),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(children: <Widget>[
+                      CircleAvatar(
+                        radius: 35.0,
+                        backgroundImage: AssetImage(favorites[index].imageUrl),
+                      ),
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      Text(
+                        favorites[index].name,
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ]),
+                  );
+                },
+                itemCount: favorites.length,
+              ),
+            )
+          ],
+        ));
   }
 }

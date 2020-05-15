864ee0d55b1281e87ab60a4aa48721575ba2b1c6
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-15 10:36:00 +0530

    add text messages into chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/user_model.dart';
+import '../models/message_model.dart';
 
 class ChatScreen extends StatefulWidget {
   final User user;
@@ -12,39 +13,49 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> {
   Widget build(context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      appBar: AppBar(
-        title: Padding(
-          padding: EdgeInsets.only(left: 0.0),
-          child: Row(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 20.0,
-                backgroundImage: AssetImage(widget.user.imageUrl),
-              ),
-              SizedBox(width: 5.0),
-              Text(
-                widget.user.name,
-              ),
-            ],
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          title: Padding(
+            padding: EdgeInsets.only(left: 0.0),
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 20.0,
+                  backgroundImage: AssetImage(widget.user.imageUrl),
+                ),
+                SizedBox(width: 5.0),
+                Text(
+                  widget.user.name,
+                ),
+              ],
+            ),
           ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            ),
+          ],
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30.0),
-            topRight: Radius.circular(30.0),
-          ),
-        ),
-      ),
-    );
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                ),
+                child: ListView.builder(
+                    itemCount: chats.length,
+                    itemBuilder: (context, index) {
+                      return Text(chats[index].text);
+                    }),
+              ),
+            )
+          ],
+        ));
   }
 }

a46a5ef6d4ac73472e8de0b1df3eace25285b84d
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-16 16:57:50 +0530

    add a text field

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -12,69 +12,96 @@ class ChatScreen extends StatefulWidget {
 
 class ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe) {
+    final Container msg = Container(
+      margin: isMe
+          ? EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              left: 80.0,
+            )
+          : EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              // right: 80.0,
+            ),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * .75,
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe
+            ? BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                bottomLeft: Radius.circular(15.0),
+              )
+            : BorderRadius.only(
+                topRight: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0),
+              ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 5.0),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+    if (isMe) {
+      return msg;
+    }
     return Row(
       children: <Widget>[
-        Container(
-          margin: isMe
-              ? EdgeInsets.only(
-                  top: 8.0,
-                  bottom: 8.0,
-                  left: 80.0,
-                )
-              : EdgeInsets.only(
-                  top: 8.0,
-                  bottom: 8.0,
-                  // right: 80.0,
-                ),
-          padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
-          decoration: BoxDecoration(
-            color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
-            borderRadius: isMe
-                ? BorderRadius.only(
-                    topLeft: Radius.circular(15.0),
-                    bottomLeft: Radius.circular(15.0),
-                  )
-                : BorderRadius.only(
-                    topRight: Radius.circular(15.0),
-                    bottomRight: Radius.circular(15.0),
-                  ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                message.time,
-                style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              SizedBox(height: 5.0),
-              Text(
-                message.text,
-                style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
-          ),
-        ),
-        !isMe
-            ? IconButton(
-                icon: message.isLiked
-                    ? Icon(Icons.favorite)
-                    : Icon(Icons.favorite_border),
-                iconSize: 30.0,
-                color: message.isLiked
-                    ? Theme.of(context).primaryColor
-                    : Colors.blueGrey,
-                onPressed: () {},
-              )
-            : SizedBox.shrink(),
+        msg,
+        IconButton(
+          icon: message.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked
+              ? Theme.of(context).primaryColor
+              : Colors.blueGrey,
+          onPressed: () {},
+        )
       ],
+    );
+  }
+
+  _buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.attach_file),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 
@@ -112,28 +139,30 @@ class ChatScreenState extends State<ChatScreen> {
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
                   ),
                 ),
                 child: ClipRRect(
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
                     topRight: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
                   ),
                   child: ListView.builder(
-                      reverse: true,
-                      // scrollDirection: Axis.vertical,
-                      padding: EdgeInsets.only(top: 15.0),
-                      itemCount: messages.length,
-                      itemBuilder: (context, index) {
-                        final Message message = messages[index];
-                        final bool isMe =
-                            messages[index].sender.id == currentUser.id;
-                        return _buildMessage(message, isMe);
-                      }),
+                    reverse: true,
+                    // scrollDirection: Axis.vertical,
+                    padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      final Message message = messages[index];
+                      final bool isMe =
+                          messages[index].sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                    },
+                  ),
                 ),
               ),
-            )
+            ),
+            _buildMessageComposer(),
           ],
         ));
   }
